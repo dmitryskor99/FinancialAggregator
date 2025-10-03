0f3c87eca402c632066042efa85adec970743591
Dmitrii.Skorodumov <skorodumov@skbkontur.ru>
2025-10-03 16:08:21 +0300

    Добавил detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,4 +6,23 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.detekt) apply true
+}
+
+subprojects {
+    apply {
+        plugin("io.gitlab.arturbosch.detekt")
+    }
+
+    detekt {
+        config.setFrom(files("$rootDir/detekt-config.yml"))
+        buildUponDefaultConfig = true
+        parallel = true
+        tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+            reports {
+                html.required.set(true)
+                xml.required.set(true)
+            }
+        }
+    }
 }
